f6f5dc309f8d910274deb3c93d8b8fc5429ff942
jshwa <32717724+jshwa@users.noreply.github.com>
2018-06-06 12:38:02 -0500

    use for loop to demonstrate iterating through comment forms to add event listener, clear input after submit, use strict mode

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -1,3 +1,5 @@
+'use strict';
+
 class CommentsController {
   constructor() {
     this.$addCommentForm = $('.add-comment')
@@ -11,17 +13,19 @@ class CommentsController {
   addCommentFormListener() {
     // create comment form listener code here
     const self = this;
-    this.$addCommentForm.on('submit', function(e){
-       e.preventDefault();
-       let imageId = $(this).data().id;
-       let commentContent = $(`#comment-description-${imageId}`).val();
-       let commentObject = new Comment(imageId, commentContent);
-       self.render(commentObject);
-    })
+    for (let i = 0; i < this.$addCommentForm.length; i++) {
+      this.$addCommentForm[i].addEventListener('submit', function(e) {
+         e.preventDefault();
+         let imageId = this.dataset.id;
+         let commentContent = $(`#comment-description-${imageId}`).val();
+         let commentObject = new Comment(imageId, commentContent);
+         self.render(commentObject);
+         $(`#comment-description-${imageId}`).val("");
+      })
+    }
   }
 
   render(commentObject) {
-     console.log(commentObject.image.id)
      $(`#comments-${commentObject.image.id}`).append(commentObject.commentEl())
   }
 }
