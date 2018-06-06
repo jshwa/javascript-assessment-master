88fa1be5c0b4262bb43a04a2101f1419762783c0
jshwa <32717724+jshwa@users.noreply.github.com>
2018-06-05 21:47:38 -0500

    add event listener to trigger on submit and grab id and comment

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -5,9 +5,17 @@ class CommentsController {
 
   init() {
     // kick off controller from here
+    this.addCommentFormListener()
   }
 
   addCommentFormListener() {
     // create comment form listener code here
+    this.$addCommentForm.on('submit', function(e){
+       e.preventDefault();
+       let id = $(this).data().id;
+       let comment = $(`#comment-description-${id}`).val();
+       console.log(id);
+       console.log(comment);
+    })
   }
 }
